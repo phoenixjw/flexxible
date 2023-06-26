de798e1a7097a52b3fc134ecf40674cecfd1fbc0
phoenixjw <phoenixjw9320@gmail.com>
2023-06-26 14:58:37 +0100

    render navbar and footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import Navbar from '@/components/Navbar';
 import './globals.css';
+import Footer from '@/components/Footer';
 
 export const metadata = {
   title: 'Flexibble',
@@ -13,11 +15,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        Navbar
+        <Navbar/>
           <main>
             {children}
           </main>
-        Footer
+        <Footer />
       </body>
     </html>
   )
